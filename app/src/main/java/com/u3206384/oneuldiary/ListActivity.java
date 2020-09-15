50d2c40fda33e89acdfdea7db57cd32c6ff34cd0
Yoonji Hong <u7095932@anu.edu.au>
2020-09-16 01:18:52 +1000

    add firebaseDB

--- app/src/main/java/com/u3206384/oneuldiary/ListActivity.java
+++ app/src/main/java/com/u3206384/oneuldiary/ListActivity.java
@@ -1,5 +1,6 @@
 package com.u3206384.oneuldiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,21 +10,65 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
     ArrayList<DiaryContent> contents = new ArrayList<DiaryContent>();
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference dbRef = db.getReference("diary_content");
+    ArrayList<String> keys = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        contents.add(new DiaryContent(LocalDate.of(2020,9,15), "Create", "Text"));
+        //contents.add(new DiaryContent(LocalDate.of(2020,9,15), "Create", "Text"));
 
-        DiaryContentAdapter adapter = new DiaryContentAdapter(this, R.layout.list_item, contents);
+        //DiaryContentAdapter adapter = new DiaryContentAdapter(this, R.layout.list_item, contents);
+
+        final DiaryContentAdapter adapter = new DiaryContentAdapter(
+                this, R.layout.list_item, contents);
+
+        dbRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                DiaryContent dc = new DiaryContent(
+                        LocalDate.parse((String) dataSnapshot.child("date").getValue()),
+                        (String) dataSnapshot.child("title").getValue(),
+                        (String) dataSnapshot.child("content").getValue()
+                );
+                adapter.add(dc);
+                keys.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
@@ -40,5 +85,12 @@ public class ListActivity extends AppCompatActivity {
                     }
                 }
         );
+
+//        FirebaseDatabase db = FirebaseDatabase.getInstance();
+//        DatabaseReference dbRef = db.getReference();
+//        dbRef.child("message").setValue("It works!");
+
     }
+
+
 }
--- app/src/main/java/com/u3206384/oneuldiary/MainActivity.java
+++ app/src/main/java/com/u3206384/oneuldiary/MainActivity.java
@@ -6,16 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.LocalDate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addDiaryContentToRealtimeDB();
     }
 
     public void openList(View v) {
         Intent intent = new Intent(getApplicationContext(), ListActivity.class);
         startActivity(intent);
+    }
+
+    public void addDiaryContentToRealtimeDB(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = db.getReference("diary_content");
+
+        DiaryContent content = new DiaryContent(LocalDate.of(2020,9,15), "Create", "Text");
+        String key = dbRef.push().getKey();
+        dbRef.child(key).child("title").setValue(content.getTitle());
+        dbRef.child(key).child("content").setValue(content.getContent());
+        dbRef.child(key).child("date").setValue(content.getDate());
+
+
     }
 }
